d7c7a989b82b4b8bb7344cd6abf9c363d70d12a7
FFugi <fudifil@gmail.com>
2018-12-03 12:14:58 +0100

    02-2018 refactor

--- 2018/02/checksum.kt
+++ 2018/02/checksum.kt
@@ -1,15 +1,7 @@
 import java.io.File
 
 fun main(args : Array<String>) {
-	if (args[1] == "1") {
-		firstPart(args[0])
-	} else {
-		secondPart(args[0])
-	}
-}
-
-fun firstPart(filename : String) {
-	var lines = File(filename).readLines()
+	var lines = File(args[0]).readLines()
 	var two : Int = 0
 	var three : Int = 0
 	val defaultMap = ('a'..'z').map { it to 0 }.toMap()
@@ -20,7 +12,7 @@ fun firstPart(filename : String) {
 		}
 		var wasTwo = false
 		var wasThree = false
-		for ((letter, occurences) in map) {
+		for ((_, occurences) in map) {
 			if (occurences == 2 && !wasTwo) {
 				wasTwo = true
 				two++
@@ -30,23 +22,22 @@ fun firstPart(filename : String) {
 			}
 		}
 	}
-	println("${two * three}")
-}
+	println("First part: ${two * three}")
 
-fun secondPart(filename : String) {
-	var lines = File(filename).readLines()
-	lup@ for (one in lines) {
-		for (two in lines) {
+	lup@ 
+	for (a in lines) {
+		for (b in lines) {
 			var diff = 0
 			var lastDiff = 0
-			for (i in 0 until one.length){
-				if (one[i] != two[i]) {
+			for (i in 0 until a.length){
+				if (a[i] != b[i]) {
 					diff++	
 					lastDiff = i
 				}
 			}
 			if (diff == 1) {
-				println("${one.substring(0..lastDiff-1)}${one.substring(lastDiff+1..one.length-1)}")
+				println("Second part: ${a.substring(0..lastDiff-1)}" +  
+						"${a.substring(lastDiff+1..a.length-1)}")
 				break@lup
 			}
 		}
