3da0cfe5348f8a61ef8667e059b0b3b5928a0d82
FFugi <fudifil@gmail.com>
2018-12-05 08:11:27 +0100

    05-2018 optimization

--- 2018/05/05.kt
+++ 2018/05/05.kt
@@ -3,25 +3,28 @@ import kotlin.math.abs
 
 fun reactMe(input : String) : String {
 	var output = input
-	do {
-		val range = output.indices	
-		var wasChange = false
-		for (i in range) {
+	var range = output.indices
+	var i = 0
+	while (i < range.last) {
+		do {
+			var wasChange = false
 			if (i < range.last) {
 				if (abs(output[i] - output[i + 1]) == 32){
 					wasChange = true
-					output = output.replaceRange(i, i + 2, "  ")
+					output = output.replaceRange(i, i + 2, "")
+					range = output.indices
+					if (i > 0) { i-- }
 				}
 			}
-		}
-		output = output.filter{ it != ' ' }
-	} while (wasChange)
+		} while (wasChange)
+		i++
+	}
 	return output
 }
 
 fun main(args : Array<String>) {
 	var input = File(args[0]).readLines()[0]
-	
+
 	var values = mutableListOf<Int>()
 	for (c in 'a'..'z') {
 		val tmp = input.filter { it != c && it != c - 32 }
