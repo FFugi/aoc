0ca51d1a537b11a0e62aca7a19973f18df3fa916
FFugi <fudifil@gmail.com>
2018-12-10 13:18:55 +0100

    10-2018 refactor

--- 2018/10/10.kt
+++ 2018/10/10.kt
@@ -4,6 +4,19 @@ data class Vector2(var x : Int, var y : Int)
 
 data class Point(val pos : Vector2, val vel : Vector2)
 
+fun maxPoint() = Point(Vector2(Int.MIN_VALUE, Int.MIN_VALUE), Vector2(0, 0))
+fun minPoint() = Point(Vector2(Int.MAX_VALUE, Int.MAX_VALUE), Vector2(0, 0))
+
+fun <T> List<T>.minmax(cmp : Comparator<T>) : Pair<T, T> {
+	return this.fold( Pair(this[0], this[0]), {
+		(min, max), current ->
+			Pair(
+					if (cmp.compare(current, min) == -1) current else min,
+					if (cmp.compare(current, max) == 1) current else max
+			)
+		})
+}
+
 fun printMe(list : List<Point>, start : Vector2, stop : Vector2) {
 	val map = list.map { it.pos }.toSet()
 	for (y in start.y..stop.y) {
@@ -11,7 +24,7 @@ fun printMe(list : List<Point>, start : Vector2, stop : Vector2) {
 			if (map.contains(Vector2(x, y))) {
 				print('#') 
 			} else { 
-				print('.') 
+				print(' ') 
 			}
 		}
 		print('\n')
@@ -28,15 +41,21 @@ fun main(args : Array<String>) {
 		Point(Vector2(splited[0], splited[1]), Vector2(splited[2], splited[3]))
 	}
 	var i = 0
+
+	val cmp = Comparator<Point>({a, b -> when {
+		a.pos.y < b.pos.y -> -1
+		a.pos.y > b.pos.y -> 1
+		a.pos.x < b.pos.x -> -1
+		a.pos.x > b.pos.x -> 1
+		else -> 0
+	}})
+		
 	do {
-		val maxR = pts.maxBy { it.pos.x }?.pos?.x ?: 0
-		val maxL = pts.minBy { it.pos.x }?.pos?.x ?: 0
-		val maxTop = pts.minBy { it.pos.y }?.pos?.y ?: 0
-		val maxBot = pts.maxBy { it.pos.y }?.pos?.y ?: 0
-		val diff = maxBot - maxTop
+		val (min, max) = pts.minmax(cmp)
+		val diff = max.pos.y - min.pos.y
 		if (diff < 10) {
 			println("First part:")
-			printMe(pts, Vector2(maxL, maxTop), Vector2(maxR, maxBot))
+			printMe(pts, min.pos, max.pos)
 		}
 		pts.forEach {
 			it.pos.x += it.vel.x
