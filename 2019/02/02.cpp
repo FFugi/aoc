e23f782fb6b70b66395df241b305fc8ae1c8c556
FFugi <fudifil@gmail.com>
2019-12-02 20:38:32 +0100

    Clean 02-2019 solution

--- 2019/02/02.cpp
+++ 2019/02/02.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 using Code = std::vector<int32_t>;
 
-bool exec(size_t& ip, Code& code, bool haltIfUnknown) {
+bool exec(size_t& ip, Code& code) {
     if (ip >= code.size()) {
         return false;
     }
@@ -16,31 +16,36 @@ bool exec(size_t& ip, Code& code, bool haltIfUnknown) {
         if (ip + 3 >= code.size()) {
             return false;
         }
-        int32_t op1 = code[code[ip + 1]];
-        int32_t op2 = code[code[ip + 2]];
-        int32_t resultAddr = code[ip + 3];
+        int32_t op1Addr = code[ip + 1];
+        int32_t op2Addr = code[ip + 2];
+        int32_t resAddr = code[ip + 3];
+        if (op1Addr > code.size() ||
+            op2Addr > code.size() ||
+            resAddr > code.size())
+        {
+            return false;
+        }
+        int32_t op1 = code[op1Addr];
+        int32_t op2 = code[op2Addr];
         int32_t result;
         if (opcode == 1) {
               result = op1 + op2;    
         } else {
               result = op1 * op2;    
         }
-        code[resultAddr] = result;
+        code[resAddr] = result;
         ip += 4;
     } else {
-        if (haltIfUnknown) {
-            return false;
-        }
-        ip++;
+        return false;
     }
     return true;
 }
 
-int32_t execCode(Code& code, int32_t noun, int32_t verb, bool halt) {
+int32_t execCode(Code& code, int32_t noun, int32_t verb) {
     code[1] = noun;
     code[2] = verb;
     size_t ip = 0;
-    while (exec(ip, code, halt));
+    while (exec(ip, code));
     return code[0];
 }
 
@@ -74,12 +79,12 @@ int main(int argc, char **argv) {
         baseCode.push_back(num);
     }
 
-    cout << "Part one: " << execCode(codeCode, 12, 2, true) << endl;
+    cout << "Part one: " << execCode(codeCode, 12, 2) << endl;
 
     for (int32_t n = 0; n <= 99; n++) {
         for (int32_t v = 0; v <= 99; v++) {
             setProgram(codeCode, baseCode);
-            if (execCode(codeCode, n, v, true) == 19690720) {
+            if (execCode(codeCode, n, v) == 19690720) {
                 cout << "Part two: " << 100 * n + v << endl;
             }
         }
