50b9a4306a8f1f0ade0a545065005c39bbf5573f
FFugi <fudifil@gmail.com>
2019-12-04 09:32:54 +0100

    Clean 04-2019 solution

--- 2019/04/04.cpp
+++ 2019/04/04.cpp
@@ -6,38 +6,37 @@ using namespace std;
 
 
 pair<bool, bool> checkPasswd(uint32_t passwd) {
-	if (passwd > 999999) {
-		return { false, false };
-	}
-	uint8_t digits[6];
-	uint32_t cp = passwd;
-	for (size_t i = 6; i > 0; i--) {
-		digits[i - 1] = passwd % 10;
-		passwd /= 10;
-	}
-	uint8_t occurences[10];
-	memset(occurences, 0, 10 * sizeof(uint8_t));
-	uint8_t prev = 0;
-	for (size_t i = 0; i < 6; i++) {
-		auto digit = digits[i];
-		if (digit < prev) {
-			return { false, false };
-		}
-		occurences[digit]++;
-		prev = digit;
-	}
-	bool hasDouble = false;
-	bool hasDoubleEx = false;
-	for (auto& d : occurences) {
-		if (d >= 2) {
-			hasDouble = true;
-		}
-		if (d == 2) {
-			hasDoubleEx = true;
-		}
-	}
+    if (passwd > 999999) {
+        return { false, false };
+    }
+    uint8_t digits[6];
+    uint32_t cp = passwd;
+    for (size_t i = 6; i > 0; i--) {
+        digits[i - 1] = passwd % 10;
+        passwd /= 10;
+    }
+    uint8_t occurences[10];
+    memset(occurences, 0, 10 * sizeof(uint8_t));
+    uint8_t prev = 0;
+    for (auto& d : digits) {
+        if (d < prev) {
+            return { false, false };
+        }
+        occurences[d]++;
+        prev = d;
+    }
+    bool hasDouble = false;
+    bool hasDoubleEx = false;
+    for (auto& d : occurences) {
+        if (d >= 2) {
+            hasDouble = true;
+        }
+        if (d == 2) {
+            hasDoubleEx = true;
+        }
+    }
 
-	return { hasDouble, hasDoubleEx };
+    return { hasDouble, hasDoubleEx };
 }
 
 
@@ -51,16 +50,16 @@ int main(int argc, char **argv) {
         cout << "Could not open file!" << endl;
         return 1;
     }
-	uint32_t from, to;
-	char c;
+    uint32_t from, to;
+    char c;
     input >> from >> c >> to;
 
-	uint32_t partOne = 0;
-	uint32_t partTwo = 0;
-	for (uint32_t i = from; i <= to; i++) {
-		partOne += checkPasswd(i).first ? 1 : 0;
-		partTwo += checkPasswd(i).second ? 1 : 0;
-	}
+    uint32_t partOne = 0;
+    uint32_t partTwo = 0;
+    for (uint32_t i = from; i <= to; i++) {
+        partOne += checkPasswd(i).first ? 1 : 0;
+        partTwo += checkPasswd(i).second ? 1 : 0;
+    }
 
     cout << "Part one: " << partOne << endl;
     cout << "Part two: " << partTwo << endl;
